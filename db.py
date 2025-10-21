41a34b05d07a2bc65d32060386fddf8881097ba1
Jonas.Nicolaysen <jonic6635@uib.no>
2025-10-21 20:56:28 +0200

    added todo class using sqlmodel

--- db.py
+++ db.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 from sqlmodel import SQLModel, Field
-import uuid
-from uuid import UUID
-from datetime import datetime
+from uuid import UUID, uuid4
+from datetime import datetime, timezone
 from enum import Enum
 
 class Status(Enum):
@@ -11,8 +10,10 @@ class Status(Enum):
     done = "done"
 
 class Todo(SQLModel, table=True):
-    id: UUID
-    name: str
-    created_at: datetime
-    due_date: datetime
-    status = Status.todo
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    name: str = Field(nullable=False, max_length=30) 
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
+    due_date: datetime | None = None
+    status: Status = Field(default=Status.todo)
+
+
